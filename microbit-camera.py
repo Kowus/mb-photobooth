f0bfca489b86057de4009822f03c9e76b1015ead
Barnabas Nomo <barnabas@elite-education.org>
2017-12-07 01:24:20 +0000

    support multiple files save
    no longer overwrites saved files. checks directory for patterns and saves file accordingly. also opens file on save

--- microbit-camera.py
+++ microbit-camera.py
@@ -1,12 +1,17 @@
 from picamera import PiCamera
 from gpiozero import Button
 from time import sleep
+import glob, os
+
 button = Button(4, pull_up = False)
 
 while True:
     with PiCamera() as camera:
+        button.wait_for_press()
         camera.start_preview()
         sleep(5)
-        button.wait_for_press()
-        camera.capture('selfie.jpg')
+        n = len(glob.glob('./images/img_*.jpg'))
+        filename = './images/img_' + str(n) + '.jpg'
+        camera.capture(filename)
+        os.system('xdg-open ' + filename)
         camera.stop_preview()
